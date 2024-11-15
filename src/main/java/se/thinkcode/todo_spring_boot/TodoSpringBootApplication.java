074b5dc6b0c24205a0fce992ccfef5597952a7d6
Thomas Sundberg <thomas@thinkcode.se>
2024-11-15 10:15:38 +0100

    Added spring boot and Maven wrapper

---
+++ src/main/java/se/thinkcode/todo_spring_boot/TodoSpringBootApplication.java
@@ -0,0 +1,13 @@
+package se.thinkcode.todo_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/thinkcode/todo_spring_boot/TodoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package se.thinkcode.todo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
