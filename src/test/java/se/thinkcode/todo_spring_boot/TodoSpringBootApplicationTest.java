e912cb02d719da5916be22ee23a400d7fdf050dc
Thomas Sundberg <thomas@thinkcode.se>
2024-11-15 10:28:06 +0100

    Initiated test environment

---
+++ src/test/java/se/thinkcode/todo_spring_boot/TodoSpringBootApplicationTest.java
@@ -0,0 +1,13 @@
+package se.thinkcode.todo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TodoSpringBootApplicationTest {
+
+    @Test
+    void should_test_stuffy_things() {
+        assertThat(true).isTrue();
+    }
+}
